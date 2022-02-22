494d85164bae5903a50e550d5098260d50461f8f
Chollathit Saensaradee <huntgame2v2@gmail.com>
2022-02-22 22:40:30 +0700

    CheckPoint in room

---
+++ src/Action/Web/CheckPointAction.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Action\Web;
+
+use App\Domain\User\Service\UserFinder;
+use App\Responder\Responder;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+/**
+ * Action.
+ */
+final class CheckPointAction
+{
+    /**
+     * @var Responder
+     */
+    private $responder;
+    private $twig;
+    private $userFinder;
+    private $session;
+
+    /**
+     * The constructor.
+     *
+     * @param Responder $responder The responder
+     */
+    public function __construct(Twig $twig,UserFinder $userFinder,Session $session,Responder $responder)
+    {
+        $this->twig = $twig;
+        $this->userFinder=$userFinder;
+        $this->session=$session;
+        $this->responder = $responder;
+    }
+
+    /**
+     * Action.
+     *
+     * @param ServerRequestInterface $request The request
+     * @param ResponseInterface $response The response
+     *
+     * @return ResponseInterface The response
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        $params = (array)$request->getQueryParams();
+        
+        $viewData = [
+            
+        ];
+        
+
+        return $this->twig->render($response, 'web/checkpoint.twig',$viewData);
+    }
+}
